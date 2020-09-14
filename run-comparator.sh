ee74dce8e2ceebd0023ecd8449805fc13b6f1fe0
Mauro <mauro.pons@mercadolibre.com>
2020-09-14 11:17:30 -0300

    Changed run-comparator.sh

--- run-comparator.sh
+++ run-comparator.sh
@@ -9,8 +9,8 @@ SCOPE_1="https://read-batch_payment-methods.furyapps.io"
 SCOPE_2="https://production-reader-testscope_payment-methods-read-v2.furyapps.io"
 ARRAY_PATHS=(
 	"/Users/mpons/Documents/comparator/payment-methods/v2/1_17-08-2020_21-08-2020/202008-10-15/MELI/MLM/MLM.csv"
-	"/Users/mpons/Documents/comparator/payment-methods/v2/1_17-08-2020_21-08-2020/202008-10-15/MELI/MLM/MLM.csv"
-	"/Users/mpons/Documents/comparator/payment-methods/v2/1_17-08-2020_21-08-2020/202008-10-15/NONE/MCO/MCO.csv"
+	"/Users/mpons/Documents/comparator/payment-methods/v2/1_17-08-2020_21-08-2020/202008-10-15/MELI/MCO/MCO.csv"
+	"/Users/mpons/Documents/comparator/payment-methods/v2/1_17-08-2020_21-08-2020/202008-10-15/NONE/MLM/MLM.csv"
 	"/Users/mpons/Documents/comparator/payment-methods/v2/1_17-08-2020_21-08-2020/202008-10-15/NONE/MCO/MCO.csv"
 	)
 
