596b0ccdc8efe18b60a8fd184fd65ed6fea3efb5
Mauro <mauro.pons@mercadolibre.com>
2020-10-08 14:15:21 -0300

    run-comparator

--- run-comparator.sh
+++ run-comparator.sh
@@ -4,10 +4,10 @@
 
 ##### Constants
 
-AUTH_TOKEN="b3510ce019c623e5f81e248c3e5f68f0c7084ec3100192c6891ae8dd8fce6ef3"
+AUTH_TOKEN="081b7a27d4ff459c54ff2b7c003f37774960a089ff5adc516f3b4ddd4623cc18"
 SCOPE_1="https://read-batch_payment-methods.furyapps.io"
-SCOPE_2="https://production-reader_payment-methods-read-v2.furyapps.io"
-#SCOPE_2="https://testing-comparator_payment-methods-read-v2.furyapps.io"
+#SCOPE_2="https://production-reader_payment-methods-read-v2.furyapps.io"
+SCOPE_2="https://testing-comparator_payment-methods-read-v2.furyapps.io"
 ARRAY_PATHS=(
   "/Users/mpons/Documents/comparator/payment-methods/v2/LOTE-1/MLM/NONE/MLM-NONE.csv"
   "/Users/mpons/Documents/comparator/payment-methods/v2/LOTE-1/MLM/MELI/MLM-MELI.csv"
